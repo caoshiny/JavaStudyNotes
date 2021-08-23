82736b7b337620dc2a696bf9a2853543b8ad2279
shiny <hmwncsn@163.com>
2021-08-23 08:12:26 +0800

    2021/8/23 8:12

--- src/interf4ce/Test.java
+++ src/interf4ce/Test.java
@@ -1,6 +1,8 @@
 package interf4ce;
 
-public class Test implements Powered, Cloneable{
+import java.awt.event.MouseEvent;
+
+public class Test implements Powered, Cloneable, defaultInterface{
     public void move (double x, double y){
         System.out.println("物体移动了！");
     }
@@ -15,9 +17,21 @@ public class Test implements Powered, Cloneable{
         System.out.println("覆盖实现接口静态函数！");
     }
 
+    // 覆盖默认方法
+    public void defaultFunc() {
+        System.out.println("覆盖defaultInterface的默认方法");
+    }
+
     public static void main(String[] args) {
         Test test = new Test();
         test.milesPerGallon();
+
+        //
+        test.defaultFunc();
+        defaultInterface defaultInterface = new Test();
+        defaultInterface.defaultFunc();
+
+
         Powered.printConsole();
         Test.printConsole();
     }
--- src/interf4ce/defaultInterface.java
+++ src/interf4ce/defaultInterface.java
@@ -6,9 +6,15 @@ import java.awt.event.MouseEvent;
  * 把声名的所有方法设置为默认方法，只需覆盖真正关心的事件
  */
 public interface defaultInterface {
-    default void mouseClicked(MouseEvent event) {}
+    default void mouseClicked(MouseEvent event) {
+        System.out.println("发生了鼠标点击事件");
+    }
     default void mousePressed(MouseEvent event) {}
     default void mouseRelease(MouseEvent event) {}
     default void mouseEnter(MouseEvent event) {}
     default void mouseExited(MouseEvent event) {}
+
+    default void defaultFunc () {
+        System.out.println("调用defaultInterface的默认方法");
+    }
 }
